8e02470eb4ae3fbdda518a45ded6ccfcaba61e53
Michael <michael_petersen_email@yahoo.com>
2020-06-05 14:10:01 -0700

    update labeling and classes for questions, begin scripting button functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $("document").ready(function() {
-  
+  $("button#start").submit(function(event) {
+    
+    $(".jumbotron").hide();
+    
+    $("form#question1").show();
+    
+    event.preventDefault();
+  });
 })
