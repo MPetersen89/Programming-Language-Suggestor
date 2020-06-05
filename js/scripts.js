104887ae1d63ff8c80bcefd9f78ae94d5e137528
Michael <michael_petersen_email@yahoo.com>
2020-06-05 14:46:26 -0700

    create const for radio selectors.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
 $("document").ready(function() {
+  const career = $("input:radio[name=career]:checked").val();
+  const task = $("input:radio[name=task]:checked").val();
+  const company = $("input:radio[name=company]:checked").val();
+  const difficulty = $("input:radio[name=difficulty]:checked").val();
+  const income = $("input:radio[name=income]:checked").val();
+  const versatility = $("input:radio[name=versatility]:checked").val();
+  const type = $("input:radio[name=type]:checked").val();
+
   $("button#start").submit(function(event) {
-    
-    $(".jumbotron").hide();
-    
-    $("form#question1").show();
-    
-    event.preventDefault();
+    $(".jumbotron").toggle()
+    $("form#questions#question0").toggle();
+    event.preventDefault();    
   });
+
+  if 
 })
