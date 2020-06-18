38553c3def23def2e625e028451ddfc16d00e1cc
Michael <michael_petersen_email@yahoo.com>
2020-06-18 08:09:06 -0700

    fix questions functionality to properly respond and show appropriate results.

--- js/scripts.js
+++ js/scripts.js
@@ -6,67 +6,44 @@ $("document").ready(function() {
     event.preventDefault()
   });
 
-  $("submit#submitButton6").submit(function(event) {
-  const career = parseInt($("input:radio[name=career]:checked").val());
-  const task = parseInt($("input:radio[name=task]:checked").val());
-  const company = parseInt($("input:radio[name=company]:checked").val());
-  const difficulty = parseInt($("input:radio[name=difficulty]:checked").val());
-  const income = parseInt($("input:radio[name=income]:checked").val());
-  const versatility = parseInt($("input:radio[name=versatility]:checked").val());
-  const type = parseInt($("input:radio[name=type]:checked").val());
-  let results = career + task + company + difficulty + income + versatility + type;
-  event.preventDefault();
+  $("#submitButton").click(function(event) {
+    event.preventDefault();
+    $("#questions").toggle();
+    const career = parseInt($("input:radio[name=optionsCareer]:checked").val());
+    const task = parseInt($("input:radio[name=optionsTask]:checked").val());
+    const company = parseInt($("input:radio[name=optionsCompany]:checked").val());
+    const difficulty = parseInt($("input:radio[name=optionsDifficulty]:checked").val());
+    const income = parseInt($("input:radio[name=optionsIncome]:checked").val());
+    const versatility = parseInt($("input:radio[name=optionsVersatility]:checked").val());
+    const type = parseInt($("input:radio[name=optionsType]:checked").val());
+    // let results = career + task + company + difficulty + income + versatility + type;
+    let results = 60
+    if (results <= 10) {
+      $("#c").show();
+    } else if (results >= 11 && results <= 15) {
+      $("#cPlusPlus").show();
+    } else if (results >= 16 && results <= 20) {
+      $("#cSharp").show();
+    } else if (results >= 21 && results <= 25) {
+      $("#css").show();
+    } else if (results >= 26 && results <= 30) {
+      $("#python").show();
+    } else if (results >= 31 && results <= 35) {
+      $("#ruby").show();
+    } else if (results >= 36 && results <= 40) {
+      $("#java").show();
+    } else if (results >= 41 && results <= 45) {
+      $("#javascript").show();
+    } else if (results >= 46 && results <= 50) {
+      $("#html").show();
+    } else if (results >= 51 && results <= 55) {
+      $("#sql").show();
+    } else if (results >= 56 && results <= 60) {
+      $("#swift").show();
+    } else if (results >= 61 && results <= 65) {
+      $("#perl").show();
+    } else if (results === 70) {
+      $("#PHP").show();
+    }
   });
-  
-  if (results <= 10) {
-    $("c").show();
-  }
-
-  if (results >= 11 && results <= 15) {
-    $("cPlusPlus").show();
-  }
-
-  if (results >= 16 && results <= 20) {
-    $("cSharp").show();
-  }
-
-  if (results >= 21 && results <= 25) {
-    $("css").show();
-  }
-
-  if (results >= 26 && results <= 30) {
-    $("python").show();
-  }
-
-  if (results >= 31 && results <= 35) {
-    $("ruby").show();
-  }
-
-  if (results >= 36 && results <= 40) {
-    $("java").show();
-  }
-
-  if (results >= 41 && results <= 45) {
-    $("javascript").show();
-  }
-
-  if (results >= 46 && results <= 50) {
-    $("html").show();
-  }
-
-  if (results >= 51 && results <= 55) {
-    $("sql").show();
-  }
-
-  if (results >= 56 && results <= 60) {
-    $("swift").show();
-  }
-
-  if (results >= 61 && results <= 65) {
-    $("perl").show();
-  }
-
-  if (results = 70) {
-    $("personalHomePage").show();
-  }
 })
